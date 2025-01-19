a900af15d310d4cf38d259c5e382cc4e714e6bad
gmurad97 <murad.dev@bk.ru>
2025-01-19 19:07:20 +0400

    first step

---
+++ build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let firstName = "Murad";
+let lastName = "Gazymagomedov";
+let year = 1997;
+let is_active = true;
+let id = 123456789123456887744n;
+const uniqueKey = Symbol("description");
+let tuple;
+tuple = ["string", true, 5];
